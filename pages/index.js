b01c1a802b60b5df738f8126525b9d2170e83a7f
Yousef Omar <yousefomar724@gmail.com>
2022-10-18 20:19:38 +0200

    refactor - add tailwindcss

--- pages/index.js
+++ pages/index.js
@@ -2,104 +2,124 @@ import Head from "next/head"
 import { useState } from "react"
 
 export default function Home() {
-  const [formErrors, setFormErrors] = useState()
+  const [formErrors, setFormErrors] = useState({})
   const [isSubmited, setIsSubmited] = useState(false)
-  const [email, setEmail] = useState()
-  const [password, setPassword] = useState()
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [run, setRun] = useState(true)
+  const [isLeft, setIsLeft] = useState(true)
+
   const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/
   const emailRegex = /^\S+@\S+\.\S+$/
 
-  const emailOnchange = (event) => {
-    const userEmail = event.target.value
-    if (!userEmail) {
-      setFormErrors((prev) => ({ ...prev, email: "Email is required" }))
-    }
-    if (!emailRegex.test(userEmail)) {
-      setFormErrors((prev) => ({ ...prev, email: "Email is Invalid" }))
-    } else {
-      setFormErrors((prev) => ({ ...prev, email: "" }))
-    }
-    setEmail(userEmail)
-  }
-  const passwordOnchange = (event) => {
-    const userPassword = event.target.value
-    if (!userPassword) {
-      setFormErrors((prev) => ({ ...prev, password: "Password is required" }))
-    } else if (!passwordRegex.test(userPassword)) {
+  const onChange = (name, value, regex, setState) => {
+    if (!value) {
       setFormErrors((prev) => ({
         ...prev,
-        password: "Password is NOT strong enough",
+        [name]: `${name} is required`,
+      }))
+    } else if (!regex.test(value)) {
+      setFormErrors((prev) => ({
+        ...prev,
+        [name]: `${name} is invalid`,
       }))
     } else {
-      setFormErrors((prev) => ({ ...prev, password: "" }))
+      setFormErrors((prev) => ({ ...prev, [name]: "" }))
     }
-    setPassword(userPassword)
+    setState(value)
   }
 
-  const handleSubmit = (event) => {
+  const onMouseEnter = () => {
+    if (formErrors?.email === "" && formErrors?.password === "") {
+      setRun(false)
+    } else {
+      setRun(true)
+      setIsLeft(!isLeft)
+    }
+  }
+
+  const onSubmit = (event) => {
     event.preventDefault()
     setIsSubmited(true)
   }
 
   return (
-    <>
+    <div className="flex flex-col gap-8 items-center justify-center text-center h-screen">
       <Head>
         <title>Create Next App</title>
       </Head>
-      <form className="form" onSubmit={handleSubmit}>
-        <div>
+      <form
+        className="flex flex-col gap-4 items-center justify-center"
+        onSubmit={onSubmit}
+      >
+        <div className="flex flex-col gap-1">
           <label htmlFor="email">Email</label>
           <input
             type="text"
             id="email"
             name="email"
             placeholder="Ex: Example@example.com"
-            style={formErrors?.email ? { border: "2px solid red" } : {}}
-            onChange={emailOnchange}
+            className={`border-2 ${
+              formErrors?.email ? "border-red-600" : "border-green-500"
+            } p-2 outline-none`}
+            onChange={(e) =>
+              onChange("email", e.target.value, emailRegex, setEmail)
+            }
             value={email}
           />
           {formErrors?.email && (
-            <small style={{ color: "red" }}>{formErrors.email}</small>
+            <small className="text-xs text-red-600">{formErrors.email}</small>
           )}
         </div>
-        <div>
-          <label htmlFor="password">Password</label>
+        <div className="flex flex-col gap-1">
+          <label htmlFor="password" className="">
+            Password
+          </label>
           <input
             type="password"
             id="password"
             name="password"
-            style={formErrors?.password ? { border: "2px solid red" } : {}}
-            onChange={passwordOnchange}
+            placeholder="********"
+            className={`border-2 ${
+              formErrors?.password ? "border-red-600" : "border-green-500"
+            } p-2 outline-none`}
+            onChange={(e) =>
+              onChange("password", e.target.value, passwordRegex, setPassword)
+            }
             value={password}
           />
           {formErrors?.password && (
-            <small style={{ color: "red" }}>{formErrors.password}</small>
+            <small className="text-xs text-red-600">
+              {formErrors.password}
+            </small>
           )}
         </div>
         <button
-          className="button left-right"
-          id="submit"
-          style={
+          className={`${
             formErrors?.email === "" && formErrors?.password === ""
-              ? { backgroundColor: "lightblue", color: "black" }
-              : { backgroundColor: "red", color: "white", transition: ".5s" }
-          }
-          onMouseEnter={() =>
-            !(formErrors?.email === "" && formErrors?.password === "")
-              ? document.getElementById("submit").classList.toggle("left-right")
-              : (document.getElementById("submit").style.alignSelf = "center")
-          }
+              ? "bg-green-500 text-gray-800"
+              : "bg-red-600 text-white"
+          } px-5 py-2 rounded-xl ${
+            run
+              ? isLeft
+                ? "self-end"
+                : "self-start"
+              : isLeft
+              ? "self-end"
+              : "self-start"
+          }`}
+          onMouseEnter={onMouseEnter}
         >
           Submit
         </button>
       </form>
 
-      {isSubmited && (
-        <div className="result">
-          <h1>{email}</h1>
-          <h1>{password}</h1>
+      {isSubmited && formErrors?.email === "" && formErrors?.password === "" && (
+        <div>
+          <h1 className="text-2xl">{email}</h1>
+          <h1 className="text-2xl">{password}</h1>
         </div>
       )}
-    </>
+    </div>
   )
 }
